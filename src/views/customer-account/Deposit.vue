500f59712b5db87138f519ce725b2c1a85e36ee6
shinerain <529546542@qq.com>
2017-10-19 00:57:05 +0800

    update

--- src/views/customer-account/Deposit.vue
+++ src/views/customer-account/Deposit.vue
@@ -9,7 +9,7 @@
         <div class="m-wthdrawals">
           <div class="form-item">
             <label class="form-label">押金金额</label>
-            <span class="form-val"><span class="unit">¥</span>{{entity.deposit_cash}}</span>
+            <span class="form-val"><span class="unit">¥</span>{{deposit}}</span>
           </div>
         </div>
         <div class="form-tip"><i class="icon"></i>点击充值押金，代表已接受<span class="agree">《押金充值协议》</span></div>
@@ -29,7 +29,8 @@
   export default {
     data() {
       return {
-        entity : {}
+        entity : {},
+        deposit : 29
       }
     },
     methods: {
@@ -40,7 +41,8 @@
         let self = this;
         s_layer.loading('请稍等...');
         Request.post('/api/customer-payment',{
-          amt : self.entity.deposit_cash,
+//          amt : self.entity.deposit_cash,
+          amt : self.deposit,
           payment_channel : 1,
           type : 2,
         },function (data) {
@@ -54,7 +56,8 @@
     mounted() {
       let self = this;
       Request.get('/api/price',{},function (data) {
-        self.entity = data;
+//        self.entity = data[0];
+        self.deposit = data[0];
       })
     }
   }
